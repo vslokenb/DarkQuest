394b2e86eb0bdf4f81444636c6a4ac757cebfcc3
vslokenb <vslokenb@cern.ch>
2025-03-11 14:12:12 -0500

    acceptance update

--- acceptance_jpsi.py
+++ acceptance_jpsi.py
@@ -32,7 +32,7 @@ int isValidCount(RVec<int> hit_detID) {
         }
     }
     // Check if event valid 
-    if ((detID_41_42 >= 1) && (detID_18_31 >= 4)) {
+    if ((detID_41_42 >= 2) && (detID_18_31 >= 8)) {
         return 1;
     }
     else {
@@ -51,7 +51,7 @@ def cal_accept(file_names,bins = [-300,42]):
         h_bin = bins[i+1]
         rdf_cut = rdf.Filter(f"truthtrack_z_vtx[0] > {l_bin} && truthtrack_z_vtx[0] < {h_bin}")
         N_tot = rdf_cut.Count().GetValue()
-        print(N_tot)
+        #print(N_tot)
         #N_accept = rdf_cut.Count().GetValue()
         if N_tot == 0:
             eff = 0
@@ -69,10 +69,10 @@ bins = [-300, -250, -200, -150, -100, -50, 0, 50, 100, 150, 200, 250, 300, 350, 
     #colors = ['b', 'g', 'r', 'c']  # Colors for different directories
     #labels = ['0cm', '-50cm', '-100cm', '-200cm']  # Labels for the directories
     #for i, (bin_resols, xaxis) in enumerate(zip(all_bin_resols, all_xaxis)):
-files=[f"/seaquest/users/xinlongl/semi-persistent/geom_change/m0_std_{args.type}_{args.nominal}/reco_standard_mu*.root",
-       f"/seaquest/users/xinlongl/semi-persistent/geom_change/m100_std_{args.type}_{args.nominal}/reco_standard_mu*.root",
-       f"/seaquest/users/xinlongl/semi-persistent/geom_change/m200_std_{args.type}_{args.nominal}/reco_standard_mu*.root",
-       f"/seaquest/users/xinlongl/semi-persistent/geom_change/m0_std_original_{args.nominal}/reco_standard_mu*.root"]
+files=[f"/seaquest/users/xinlongl/semi-persistent/geom_change/m0_std_{args.type}_{args.nominal}/reco_standard_JPsi*.root",
+       f"/seaquest/users/xinlongl/semi-persistent/geom_change/m100_std_{args.type}_{args.nominal}/reco_standard_JPsi*.root",
+       f"/seaquest/users/xinlongl/semi-persistent/geom_change/m200_std_{args.type}_{args.nominal}/reco_standard_JPsi*.root",
+       f"/seaquest/users/xinlongl/semi-persistent/geom_change/m0_std_original_{args.nominal}/reco_standard_JPsi*.root"]
 axis=[-275,-225,-175,-125,-75,-25,25,75,125,175,225,275,325,375,425,475,525,575]
 labels=['0cm shift','100cm shift','200cm shift', 'nominal']
 colors=['b','g','r','c']
@@ -86,10 +86,10 @@ plt.xlabel('truth z vtx')
 plt.ylabel('fractional acceptance')
 plt.ylim(0,1)
 plt.legend()
-plt.title('Muon acceptance (std tracking)')
+plt.title('J/Psi acceptance (std tracking)')
 
 
 
 # Save and show the plot
-plt.savefig(f'fast_accept_mu_{args.type}_{args.nominal}.pdf', format='pdf')
+plt.savefig(f'fast_accept_jpsi_{args.type}_{args.nominal}.pdf', format='pdf')
 plt.show()
